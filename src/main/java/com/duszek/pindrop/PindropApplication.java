444f32fd13539822dca40fa069019cac939d2cb9
Kacper <nitubydz@gmail.com>
2026-06-15 12:06:28 +0200

    Initial commit

---
+++ src/main/java/com/duszek/pindrop/PindropApplication.java
@@ -0,0 +1,13 @@
+package com.duszek.pindrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PindropApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PindropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duszek/pindrop/PindropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duszek.pindrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PindropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
